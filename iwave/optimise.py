0b9170f6386755ccd40c33b4e4ffe6fff19fbf24
dolcettig <g.dolcetti8@gmail.com>
2024-10-06 12:08:40 +0200

    Created optimisation function optimise.py and corresponding test_optimise

---
+++ iwave/optimise.py
@@ -0,0 +1,439 @@
+import numpy as np
+from typing import Tuple
+from iwave import dispersion, spectral
+from scipy import optimize
+
+def optimise_velocity(
+        measured_spectrum: np.ndarray,
+        bnds: Tuple[Tuple[float, float], Tuple[float, float]],
+        depth: float,
+        vel_indx: float,
+        window_dims: Tuple[int, int, int], 
+        kt: np.ndarray,
+        ky: np.ndarray,
+        kx: np.ndarray,
+        res: float, 
+        fps: float,
+        velocity_threshold: float,
+        spectrum_threshold: float=1,
+        gauss_width: float=1,
+        gravity_waves_switch: bool=True,
+        turbulence_switch: bool=True,
+) -> Tuple[float, float, float]:
+    """
+    Pre-processes the measured spectrum, 
+    then runs the optimisation to calculate the optimal velocity components
+
+    Parameters
+    ----------
+    measured_spectrum : np.ndarray
+        measured and averaged 3D power spectrum calculated with spectral.py
+
+    bnds : [(float, float), (float, float)]
+        [(min_vel_y, max_vel_y), (min_vel_x, max_vel_x)] velocity bounds (m/s)
+
+    depth : float
+        water depth (m)
+
+    vel_indx : float
+        surface velocity to depth-averaged-velocity index (-)
+
+    window_dims: [int, int, int]
+        [dim_t, dim_y, dim_x] window dimensions
+        
+    kt: np.ndarray
+        radian frequency vector (rad/s)
+
+    ky: np.ndarray
+        y-wavenumber vector (rad/m)
+    
+    kx: np.ndarray
+        x-wavenumber vector (rad/m)
+
+    res: float
+        image resolution (m/pxl)
+
+    fps: float
+        image acquisition rate (fps)
+    
+    velocity_threshold: float
+        maximum surface velocity for spectrum filtering (m/s).
+
+    spectrum_threshold: float=1
+        threshold parameter for spectrum filtering. 
+        The spectrum with amplitude < threshold_preprocessing * mean(measured_spectrum) is filtered out.
+        threshold_preprocessing < 1 yields a more severe filtering but could eliminate part of useful signal.
+
+    gauss_width: float=1
+        width of the synthetic spectrum smoothing kernel.
+        gauss_width > 1 could be useful with very noisy spectra.
+
+    gravity_waves_switch: bool=True
+        if True, gravity waves are modelled
+        if False, gravity waves are NOT modelled
+
+    turbulence_switch: bool=True
+        if True, turbulence-generated patterns and/or floating particles are modelled
+        if False, turbulence-generated patterns and/or floating particles are NOT modelled
+
+    Returns
+    -------
+    optimised_velocity_y : float
+        optimised y velocity component (m/s)
+
+    optimised_velocity_x : float
+        optimised x velocity component (m/s)
+        
+    optimised_cost_function : float
+        cost_function calculated with optimised velocity components
+    """
+
+    # pre-processing of the measured spectrum
+    measured_spectrum = spectrum_preprocessing(measured_spectrum, kt, ky, kx, velocity_threshold, spectrum_threshold)
+
+    # optimisation
+    opt = optimize.differential_evolution(
+        cost_function_velocity,
+        bounds=bnds,
+        args=(measured_spectrum, depth, vel_indx, window_dims, res, fps, gauss_width, gravity_waves_switch, turbulence_switch),
+    )
+
+    # optimised parameters and cost function
+    optimised_velocity_y = opt.x[0]
+    optimised_velocity_x = opt.x[1] 
+    optimised_cost_function = opt.fun
+    
+    return optimised_velocity_y, optimised_velocity_x, optimised_cost_function
+
+def cost_function_velocity(
+        x: Tuple[float, float],
+        measured_spectrum: np.ndarray,
+        depth: float,
+        vel_indx: float,
+        window_dims: Tuple[int, int, int], 
+        res: float, 
+        fps: float,
+        gauss_width: float,
+        gravity_waves_switch: bool=True,
+        turbulence_switch: bool=True,
+) -> float: 
+    """
+    Creates a synthetic spectrum based on guessed parameters, 
+    then compares it with the measured spectrum and returns a cost function for minimisation
+
+    Parameters
+    ----------
+    x :  [float, float]
+        velocity_y x velocity_x
+        tentative surface velocity components along y and x (m/s)
+
+    measured_spectrum : np.ndarray
+        measured, averaged, and normalised 3D power spectrum calculated with spectral.py
+
+    depth : float
+        tentative water depth (m)
+
+    vel_indx : float
+        surface velocity to depth-averaged-velocity index (-)
+
+    window_dims: [int, int, int]
+        [dim_t, dim_y, dim_x] window dimensions
+
+    res: float
+        image resolution (m/pxl)
+
+    fps: float
+        image acquisition rate (fps)
+    
+    gauss_width: float
+        width of the synthetic spectrum smoothing kernel
+
+    gravity_waves_switch: bool=True
+        if True, gravity waves are modelled
+        if False, gravity waves are NOT modelled
+
+    turbulence_switch: bool=True
+        if True, turbulence-generated patterns and/or floating particles are modelled
+        if False, turbulence-generated patterns and/or floating particles are NOT modelled
+
+    Returns
+    -------
+    cost_function : float
+        cost function to be minimised
+
+    """
+    
+    vel_y = x[0]    # guessed y velocity component
+    vel_x = x[1]    # guessed x velocity component
+
+    velocity = [vel_y, vel_x]    # guessed velocity components
+
+    # calculate the synthetic spectrum based on the guess velocity
+    synthetic_spectrum = dispersion.intensity(velocity, depth, vel_indx, window_dims, res, fps, gauss_width, gravity_waves_switch, turbulence_switch)
+
+    cost_function = nsp_inv(measured_spectrum, synthetic_spectrum)
+
+    return cost_function
+
+def optimise_velocity_depth(
+        measured_spectrum: np.ndarray,
+        bnds: Tuple[Tuple[float, float], Tuple[float, float], Tuple[float, float]],
+        vel_indx: float,
+        window_dims: Tuple[int, int, int], 
+        kt: np.ndarray,
+        ky: np.ndarray,
+        kx: np.ndarray,
+        res: float, 
+        fps: float,
+        velocity_threshold: float,
+        spectrum_threshold: float=1,
+        gauss_width: float=1,
+        gravity_waves_switch: bool=True,
+        turbulence_switch: bool=True,
+) -> Tuple[float, float, float, float]:
+    """
+    Pre-processes the measured spectrum, 
+    then runs the optimisation to calculate the optimal velocity components
+
+    Parameters
+    ----------
+    measured_spectrum : np.ndarray
+        measured and averaged 3D power spectrum calculated with spectral.py
+
+    bnds : [(float, float), (float, float), (float, float)]
+        [(min_vel_y, max_vel_y), (min_vel_x, max_vel_x), (min_depth, max_depth)] velocity (m/s) and depth (m) bounds
+
+    vel_indx : float
+        surface velocity to depth-averaged-velocity index (-)
+
+    window_dims: [int, int, int]
+        [dim_t, dim_y, dim_x] window dimensions
+        
+    kt: np.ndarray
+        radian frequency vector (rad/s)
+
+    ky: np.ndarray
+        y-wavenumber vector (rad/m)
+    
+    kx: np.ndarray
+        x-wavenumber vector (rad/m)
+
+    res: float
+        image resolution (m/pxl)
+
+    fps: float
+        image acquisition rate (fps)
+    
+    velocity_threshold: float
+        maximum surface velocity for spectrum filtering (m/s).
+
+    spectrum_threshold: float=1
+        threshold parameter for spectrum filtering. 
+        The spectrum with amplitude < threshold_preprocessing * mean(measured_spectrum) is filtered out.
+        threshold_preprocessing < 1 yields a more severe filtering but could eliminate part of useful signal.
+
+    gauss_width: float=1
+        width of the synthetic spectrum smoothing kernel.
+        gauss_width > 1 could be useful with very noisy spectra.
+
+    gravity_waves_switch: bool=True
+        if True, gravity waves are modelled
+        if False, gravity waves are NOT modelled
+
+    turbulence_switch: bool=True
+        if True, turbulence-generated patterns and/or floating particles are modelled
+        if False, turbulence-generated patterns and/or floating particles are NOT modelled
+
+    Returns
+    -------
+    optimised_velocity_y : float
+        optimised y velocity component (m/s)
+
+    optimised_velocity_x : float
+        optimised x velocity component (m/s)
+        
+    optimised_depth : float
+        optimised depth (m)
+
+    optimised_cost_function : float
+        cost_function calculated with optimised velocity components
+    """
+    # pre-processing of the measured spectrum
+    measured_spectrum = spectrum_preprocessing(measured_spectrum, kt, ky, kx, velocity_threshold, spectrum_threshold)
+
+    # optimisation
+    opt = optimize.differential_evolution(
+        cost_function_velocity_depth,
+        bounds=bnds,
+        args=(measured_spectrum, vel_indx, window_dims, res, fps, gauss_width, gravity_waves_switch, turbulence_switch),
+    )
+
+    # optimised parameters and cost function
+    optimised_velocity_y = opt.x[0]
+    optimised_velocity_x = opt.x[1] 
+    optimised_depth = opt.x[2]
+    optimised_cost_function = opt.fun
+    
+    return optimised_velocity_y, optimised_velocity_x, optimised_depth, optimised_cost_function
+
+def cost_function_velocity_depth(
+        x: Tuple[float, float, float],
+        measured_spectrum: np.ndarray,
+        vel_indx: float,
+        window_dims: Tuple[int, int, int], 
+        res: float, 
+        fps: float,
+        gauss_width: float,
+        gravity_waves_switch: bool=True,
+        turbulence_switch: bool=True,
+) -> float: 
+    """
+    Creates a synthetic spectrum based on guessed parameters, 
+    then compares it with the measured spectrum and returns a cost function for minimisation
+
+    Parameters
+    ----------
+    x :  [float, float, float]
+        velocity_y, velocity_x, depth
+        tentative surface velocity components along y and x (m/s) and depth (m)
+
+    measured_spectrum : np.ndarray
+        measured, averaged, and normalised 3D power spectrum calculated with spectral.py
+
+    vel_indx : float
+        surface velocity to depth-averaged-velocity index (-)
+
+    window_dims: [int, int, int]
+        [dim_t, dim_y, dim_x] window dimensions
+
+    res: float
+        image resolution (m/pxl)
+
+    fps: float
+        image acquisition rate (fps)
+    
+    gauss_width: float
+        width of the synthetic spectrum smoothing kernel
+
+    gravity_waves_switch: bool=True
+        if True, gravity waves are modelled
+        if False, gravity waves are NOT modelled
+
+    turbulence_switch: bool=True
+        if True, turbulence-generated patterns and/or floating particles are modelled
+        if False, turbulence-generated patterns and/or floating particles are NOT modelled
+
+    Returns
+    -------
+    cost_function : float
+        cost function to be minimised
+
+    """
+    
+    vel_y = x[0]    # guessed y velocity component
+    vel_x = x[1]    # guessed x velocity component
+    depth = x[2]    # guessed depth
+
+    velocity = [vel_y, vel_x]    # guessed velocity components
+
+    # calculate the synthetic spectrum based on the guess velocity
+    synthetic_spectrum = dispersion.intensity(velocity, depth, vel_indx, window_dims, res, fps, gauss_width, gravity_waves_switch, turbulence_switch)
+
+    cost_function = nsp_inv(measured_spectrum, synthetic_spectrum)
+
+    return cost_function
+
+def nsp_inv(
+        measured_spectrum: np.ndarray,
+        synthetic_spectrum: np.ndarray
+) -> float:
+    """
+    Combine the measured and synthetic spectra and calculate the cost function (inverse of the normalised scalar product)
+
+    Parameters
+    ----------
+    measured_spectrum : np.ndarray
+        measured, averaged, and normalised 3D power spectrum calculated with spectral.py
+
+    synthetic_spectrum: np.ndarray
+        synthetic 3D power spectrum
+
+    Returns
+    -------
+    cost : float
+        cost function to be minimised
+
+    """
+
+    measured_spectrum = measured_spectrum / np.sum(measured_spectrum) # normalise measured spectrum
+    synthetic_spectrum = synthetic_spectrum / np.sum(synthetic_spectrum) # normalised synthetic spectrum
+
+    spectra_correlation = measured_spectrum * synthetic_spectrum # calculate correlation
+
+    cost = 1 / np.sum(spectra_correlation) # calculate cost function
+
+    return cost
+
+def spectrum_preprocessing(
+        measured_spectrum: np.ndarray, 
+        kt: np.ndarray,
+        ky: np.ndarray,
+        kx: np.ndarray,
+        velocity_threshold: float,
+        spectrum_threshold: float=1
+) -> np.ndarray:
+    """
+    pre-processing of the measured spectrum to improve convergence of the optimisation
+
+    Parameters
+    ----------
+    measured_spectrum : np.ndarray
+        measured, averaged, and normalised 3D power spectrum calculated with spectral.py
+
+    kt: np.ndarray
+        radian frequency vector (rad/s)
+
+    ky: np.ndarray
+        y-wavenumber vector (rad/m)
+    
+    kx: np.ndarray
+        x-wavenumber vector (rad/m)
+
+    velocity_threshold: float
+        maximum threshold velocity for spectrum filtering (m/s).
+    
+    spectrum_threshold: float=1
+        threshold parameter for spectrum filtering. 
+        the spectrum with amplitude < threshold_preprocessing * mean(measured_spectrum) is filtered out.
+        threshold_preprocessing < 1 yields a more severe filtering but could eliminate part of useful signal.
+
+    Returns
+    -------
+    preprocessed_spectrum : np.ndarray
+        pre-processed and normalised measured 3D spectrum
+
+    """
+    
+    # spectrum normalisation: divides the spectrum at each frequency by the average across all wavenumber combinations at the same frequency
+    preprocessed_spectrum = measured_spectrum / np.mean(measured_spectrum, axis=(1, 2), keepdims=True)
+
+    # apply threshold
+    threshold = spectrum_threshold 
+    preprocessed_spectrum[preprocessed_spectrum < threshold] = 0
+
+    # set the first slice (frequency=0) to 0
+    preprocessed_spectrum[0,:,:] = 0
+
+    # calculate the threshold frequency based on threshold velocity
+    kt_gw, kt_turb = dispersion.dispersion(ky, kx, [velocity_threshold, velocity_threshold], 100, 1) # calculate frequency from velocity
+
+    # set all frequencies higher than the threshold frequency to 0
+    kt_reshaped = kt[:, np.newaxis, np.newaxis] # reshape kt to be broadcastable
+    kt_turb_bc = np.broadcast_to(kt_turb, (kt.shape[0], kt_turb.shape[1], kt_turb.shape[2])) # broadcast kt_turb to match the dimensions of kt
+    kt_bc = np.broadcast_to(kt_reshaped, kt_turb_bc.shape) # broadcast kt to match the dimensions of kt_turb
+    preprocessed_spectrum[kt_bc > kt_turb_bc] = 0 # apply mask
+
+    # remove NaNs
+    preprocessed_spectrum = np.nan_to_num(preprocessed_spectrum)
+
+    return preprocessed_spectrum
---
+++ tests/test_optimise.py
@@ -0,0 +1,131 @@
+import numpy as np
+import pytest
+from iwave import spectral, dispersion, optimise
+
+def test_preprocessing():
+    img = np.random.rand(32, 32, 32)
+    kt, ky, kx = spectral.wave_numbers(img.shape, res=0.02, fps=25)
+    kt_gw, kt_turb = dispersion.dispersion(ky, kx, velocity=[1, 0], depth=1, vel_indx=1)
+    synthetic_spectrum = dispersion.theoretical_spectrum(kt_gw, kt_turb, kt, gauss_width=1, gravity_waves_switch=True, 
+                                                         turbulence_switch=True)
+    measured_spectrum = spectral._numpy_fourier_transform(img)
+    preprocessed_spectrum = optimise.spectrum_preprocessing(measured_spectrum, kt, ky, kx, velocity_threshold=5)
+    #test if the size of the preprocessed spectrum matches the one of the theoretical spectrum
+    assert preprocessed_spectrum.shape == synthetic_spectrum.shape
+
+
+def test_nsp(img_size = (256, 64, 64), res = 0.02, fps = 25):
+    kt, ky, kx = spectral.wave_numbers(img_size, res, fps)
+    kt_gw, kt_turb = dispersion.dispersion(ky, kx, velocity=[1, 0], depth=1, vel_indx=1)
+    synthetic_spectrum = dispersion.theoretical_spectrum(kt_gw, kt_turb, kt, gauss_width=1, gravity_waves_switch=True, 
+                                                         turbulence_switch=True)
+    cost = optimise.nsp_inv(synthetic_spectrum,synthetic_spectrum)
+    expected_cost = (np.sum(synthetic_spectrum)) ** 2 / np.sum(synthetic_spectrum ** 2)
+    #test if the auto-correlation matches the theoretical expectation based on a synthetic spectrum
+    assert np.allclose(cost, expected_cost)
+
+def test_cost_function_velocity(img_size = (256, 64, 64), res = 0.02, fps = 25):
+    kt, ky, kx = spectral.wave_numbers(img_size, res, fps)
+    velocity_1 = [1, 0]
+    velocity_2 = [1.01, 0]
+    velocity_3 = [0.99, 0]
+    depth = 1
+    vel_indx=1
+    kt_gw_1, kt_turb_1 = dispersion.dispersion(ky, kx, velocity_1, depth, vel_indx)
+    synthetic_spectrum_1 = dispersion.theoretical_spectrum(kt_gw_1, kt_turb_1, kt, gauss_width=1, 
+                                                           gravity_waves_switch=True, turbulence_switch=True)
+    cost_11 = optimise.cost_function_velocity(velocity_1, synthetic_spectrum_1, depth, vel_indx, 
+                                              img_size, res, fps, gauss_width=1, 
+                                              gravity_waves_switch=True, turbulence_switch=True)
+    cost_12 = optimise.cost_function_velocity(velocity_2, synthetic_spectrum_1, depth, vel_indx, 
+                                              img_size, res, fps, gauss_width=1, 
+                                              gravity_waves_switch=True, turbulence_switch=True)
+    cost_13 = optimise.cost_function_velocity(velocity_3, synthetic_spectrum_1, depth, vel_indx, 
+                                              img_size, res, fps, gauss_width=1, 
+                                              gravity_waves_switch=True, turbulence_switch=True)
+    #test if the cost function increases when the velocity deviates from optimal
+    assert cost_12 > cost_11
+    assert cost_13 > cost_11
+    
+
+def test_optimise_velocity(img_size = (256, 64, 64), res = 0.02, fps = 25):
+    kt, ky, kx = spectral.wave_numbers(img_size, res, fps)
+    velocity = [1, 0]
+    depth = 0.3
+    velocity_indx = 1
+    kt_gw, kt_turb = dispersion.dispersion(ky, kx, velocity, depth, velocity_indx)
+    synthetic_spectrum = dispersion.theoretical_spectrum(kt_gw, kt_turb, kt, gauss_width=1, gravity_waves_switch=True, 
+                                                         turbulence_switch=True)
+    vel_y_min = 0
+    vel_y_max = 2
+    vel_x_min = -0.5
+    vel_x_max = 0.5
+    vel_threshold = 5
+    bounds = [(vel_y_min, vel_y_max), (vel_x_min, vel_x_max)]
+    optimal = optimise.optimise_velocity(synthetic_spectrum, bounds, depth, velocity_indx, img_size, kt, ky, kx,
+                                         res, fps, velocity_threshold=vel_threshold, spectrum_threshold=1, 
+                                         gauss_width=1, gravity_waves_switch=True, turbulence_switch=True)
+    vel_y_optimal = optimal[0]
+    vel_x_optimal = optimal[1]
+    assert vel_x_max >= vel_x_min
+    assert vel_y_max >= vel_y_min
+    assert np.abs(vel_y_optimal - velocity[0]) < 0.01
+    assert np.abs(vel_x_optimal - velocity[1]) < 0.01
+
+
+def test_cost_function_velocity_depth(img_size = (256, 64, 64), res = 0.02, fps = 25):
+    kt, ky, kx = spectral.wave_numbers(img_size, res, fps)
+    depth_1 = 0.30
+    depth_2 = 0.29
+    depth_3 = 0.31
+    velocity_y = 1
+    velocity_x = 0
+    params_1 = [velocity_y, velocity_x, depth_1]
+    params_2 = [velocity_y, velocity_x, depth_2]
+    params_3 = [velocity_y, velocity_x, depth_3]
+    vel_indx = 1
+    kt_gw_1, kt_turb_1 = dispersion.dispersion(ky, kx, [velocity_y, velocity_x], depth_1, vel_indx)
+    synthetic_spectrum_1 = dispersion.theoretical_spectrum(kt_gw_1, kt_turb_1, kt, gauss_width=1, gravity_waves_switch=True, 
+                                                           turbulence_switch=True)
+    cost_11 = optimise.cost_function_velocity_depth(params_1, synthetic_spectrum_1, vel_indx, 
+                                                    img_size, res, fps, gauss_width=1, 
+                                                    gravity_waves_switch=True, turbulence_switch=True)
+    cost_12 = optimise.cost_function_velocity_depth(params_2, synthetic_spectrum_1, vel_indx, 
+                                                    img_size, res, fps, gauss_width=1, 
+                                                    gravity_waves_switch=True, turbulence_switch=True)
+    cost_13 = optimise.cost_function_velocity_depth(params_3, synthetic_spectrum_1, vel_indx, 
+                                                    img_size, res, fps, gauss_width=1,
+                                                    gravity_waves_switch=True, turbulence_switch=True)
+    #test if the cost function increases when the depth deviates from optimal
+    assert cost_12 > cost_11
+    assert cost_13 > cost_11
+
+
+def test_optimise_velocity_depth(img_size = (256, 128, 128), res = 0.02, fps = 25):
+    kt, ky, kx = spectral.wave_numbers(img_size, res, fps)
+    velocity = [1, 0]
+    depth = 0.3
+    velocity_indx = 1
+    kt_gw, kt_turb = dispersion.dispersion(ky, kx, velocity, depth, velocity_indx)
+    synthetic_spectrum = dispersion.theoretical_spectrum(kt_gw, kt_turb, kt, gauss_width=1, gravity_waves_switch=True, 
+                                                         turbulence_switch=True)
+    vel_y_min = 0
+    vel_y_max = 2
+    vel_x_min = -0.5
+    vel_x_max = 0.5
+    depth_min = 0.01
+    depth_max = 1
+    vel_threshold = 5
+    bounds = [(vel_y_min, vel_y_max), (vel_x_min, vel_x_max), (depth_min, depth_max)]
+    optimal = optimise.optimise_velocity_depth(synthetic_spectrum, bounds, velocity_indx, img_size, kt, ky, kx,
+                                         res, fps, velocity_threshold=vel_threshold, spectrum_threshold=1, 
+                                         gauss_width=1, gravity_waves_switch=True, turbulence_switch=True)
+    vel_y_optimal = optimal[0]
+    vel_x_optimal = optimal[1]
+    depth_optimal = optimal[2]
+    assert vel_x_max >= vel_x_min
+    assert vel_y_max >= vel_y_min
+    assert depth_max >= depth_min
+    assert np.abs(vel_y_optimal - velocity[0]) < 0.01
+    assert np.abs(vel_x_optimal - velocity[1]) < 0.01
+    assert np.abs(depth_optimal - depth) < 0.05
